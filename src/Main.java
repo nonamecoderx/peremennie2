178dd4c2b3b7d7a05372030ca16b03326761cb73
Иван <ivankiseliov@rambler.ru>
2022-09-11 21:37:12 +0300

    Задача 5

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-     byte time = 2;
-     byte total = 16;
-     byte time1 = 20;
-     int proizv1 = total / time * time1;
-     System.out.println("За " + time1 + " минут машина произвела бутылок " + proizv1 + " штук");
-     int time2 = time1 * 3 * 24;
-     int proizv2 = time2 * total;
-     System.out.println("За " + time2 + " минут машина произвела бутылок " + proizv2 + " штук");
-     int time3 = time2 * 3;
-     int proizv3 = time3 * total;
-     System.out.println("За " + time3 + " минут машина произвела бутылок " + proizv3 + " штук");
-     int time4 = time2 * 31;
-     int proizv4 = time4 * total;
-     System.out.println("За " + time4 + " минут машина произвела бутылок " + proizv4 + " штук");
+     byte total = 120;
+     byte klassWhite = 2;
+     byte klassBrown = 4;
+     int oneClass = klassBrown + klassWhite;
+     int totalClasses = total / oneClass;
+     int totalClassWhite = totalClasses * klassWhite;
+     int totalClassBrown = totalClasses * klassBrown;
+     System.out.println("В школе, где " + totalClasses + " классов, нужно " + totalClassWhite + " банок белой краски и " + totalClassBrown + " банок коричневой краски");
     }
 }
