3bbed7daabf2753e98b3f9150822897e67cd6e3b
Иван <ivankiseliov@rambler.ru>
2022-09-12 20:36:34 +0300

    Задача 8

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-    byte vesKg = 7;
-    int vesGr = vesKg * 1000;
-    short weightOne = 250;
-    short weihgtTwo = 500;
-    int dayOne = vesGr / weightOne; //похудение при потере 250гр в день
-    int dayTwo = vesGr / weihgtTwo; //похудение при потере 500гр в день
-    int daySred = (dayTwo + dayOne) / 2; //похудение при потере 375гр в день
-    System.out.println("Если в день терять " + weightOne + " гр, то на похудение уйдет " + dayOne + " дней");
-    System.out.println("Если в день терять " + weihgtTwo + " гр, то на похудение уйдет " + dayTwo + " дней");
-    System.out.println("В среднем на похудение уйдет " + daySred + " дней");
+    int mashaOld = 67760;
+    int denOld = 83690;
+    int krisOld = 76230;
+    int mashaOldYear = mashaOld * 12;       //зп за год Маши до повышения
+    int denOldYear = denOld * 12;           //зп за год Дениса до повышения
+    int krisOldYear = krisOld * 12;         //зп за год Кристины до повышения
+    double mashaNew = mashaOld * 1.1;       //зп в месяц после повышения
+    double denNew = denOld * 1.1;           //зп в месяц после повышения
+    double krisNew = krisOld * 1.1;         //зп в месяц после повышения
+    double mashaNewYear = mashaNew * 12;    //зп за год после повышения
+    double denNewYear = denNew * 12;        //зп за год после повышения
+    double krisNewYear = krisNew * 12;      //зп за год после повышения
+    double raznMasha = mashaNewYear - mashaOldYear;
+    double raznDen = (denNewYear - denOldYear);
+    double raznKris = krisNewYear - krisOldYear;
+    System.out.println("Маша теперь получает " + mashaNew + " рублей. Годовой доход вырос на " + raznMasha + " рублей.");
+    System.out.println("Денис теперь получает " +  denNew + " рублей. Годовой доход вырос на " +  raznDen + " рублей.");
+    System.out.println("Кристина теперь получает " + krisNew + " рублей. Годовой доход вырос на " + raznKris + " рублей.");
     }
 }
